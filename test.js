288b9e57891f3c88242ced07a31defd7b4719abd
Abraham Biaye <167629707+AbrahamBiaye@users.noreply.github.com>
2025-03-20 17:01:06 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,7 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
-
-export let options = {
-    vus: 10, // 10 utilisateurs simultanés
-    duration: '30s', // Test de charge pendant 30 secondes
-};
+import { sleep } from 'k6';
 
 export default function () {
-    let res = http.get('https://test-api.com/endpoint');
-    
-    check(res, {
-        'status is 200': (r) => r.status === 200,
-        'response time < 500ms': (r) => r.timings.duration < 500,
-    });
-
-    sleep(1);
+  http.get('https://test.k6.io');
+  sleep(1);
 }
